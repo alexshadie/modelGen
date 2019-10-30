c0570d6d5c9451eaa01885b682166caac2258903
alexshadie <alex@astra.ws>
2019-10-30 23:31:09 +0300

    Added toDbArray method

--- src/ext/ToArrayTrait.php
+++ src/ext/ToArrayTrait.php
@@ -23,6 +23,28 @@ trait ToArrayTrait
         return $result;
     }
 
+    protected static function camelToUnderscore($input) {
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('_', $ret);
+    }
+
+    public function toDbArray(): array
+    {
+        $reflection = new \ReflectionClass($this);
+        $props = $reflection->getProperties();
+        $result = [];
+        foreach ($props as $prop) {
+            $prop->setAccessible(true);
+
+            $result[self::camelToUnderscore($prop->getName())] = $prop->getValue($this);
+        }
+        return $result;
+    }
+
     public function serialize(): string
     {
         return json_encode([
